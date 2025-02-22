6b6d3fd28e858316e3ec58716a1c07992b518684
hamza amiri <hamza.amiricoding@gmail.com>
2025-02-22 23:57:59 +0100

    evrything

---
+++ l/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'App.css';
+
+function App() {
+  return (
+    <div className="kids-container">
+      {/* Empty container ready for components */}
+      <div className="content-area">
+        {/* Add your components here later */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
